db0e0e486a755aaf252b4ab1becf22f43cdb97d7
AnneTvt <116728670+AnneTvt@users.noreply.github.com>
2022-12-27 11:04:42 +0100

    Update code-final.js
    Ajout de la liste de sujets
    Ajout de la fonction PhraseFinale qui remet la phrase dans l'ordre et la change de couleur
    -> à modifier pour que le texte reste rose et ne clignote pas... -> trouver un moyen d'arrêter la fonction Etape2
    Modification de la fonction Etape2 pour que les mots soient séparés sur le canva

--- code-final.js
+++ code-final.js
@@ -1,14 +1,18 @@
 //définition des fonctions
 //tableau des textes
-let sujet = ["","","","","","","","","","","","","","","","","","","",""];
+let sujet = ["La cadre","Pauline","Le petit garçon","Ma pantoufle","Le sapin de Noël","Le fauteuil","L'ornithorinque","Un tapis","Mon orteil","Le carrelage","Le pigeon","Son transat","Un avion en papier","L'ourlet de mon jean","Ma carotide interne","Son lavabo","Louis Grandjean","Francfort","Le transpalette","Ma soeur"];
 let verbe = ["s'enfuit","bougonne","épluchait","filme","s'agrandissait","tourne","roule","s'enroule","dirigea","découvrit","dormi","poussa","vole","juge","se cachait","tapa","flamba","s'allume","fume","a esquivé"];
 let complement = ["dans la forêt","sur son radeau","au magasin","rapidement","avec sa plante","sa sœur","une pomme","un carambar","dans son lit","avec amour","doucement","avec une orange","lors du réveillon","devant la mer","en voyage","au jardin","à la cave","avec grand-père","à l'Ecole de Design","dans une boîte"];
 
-//positions 
+//positions initiales du texte
 let x = [];
 let y = [];
+//deuxième position du texte
 let x2 = [];
 let y2 = [];
+//positions finales des mots de la phrase
+let xf = [200,300,400];
+let yf = [200,300,400];
 
 //liste (vide initialement) des termes
 let choisi = [];
@@ -20,13 +24,13 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     textFont("Sans-serif");
     //remplir la tableau de positions aléatoirement + choisir le terme choisi
-    for (let i=0; i<1; i++){
+    for (let i=0; i<3; i++){
         x.push(random(0, windowWidth-50));
         y.push(random(0, windowHeight-50));
         x2.push(random(0, windowWidth));
         y2.push(random(0, windowHeight));
-        choisi.push(random(verbe), random(complement));
     }
+    choisi.push(random(verbe), random(complement));
 }
 
 function draw() {
@@ -34,14 +38,18 @@ function draw() {
     //afficher la fonction texte = le texte s'affiche en blanc à un endroit aléatoire
     displayText();
     //afficher la fonction Etape2 après 1,5sec = le texte change de place et de couleur
-    setTimeout(Etape2, 1500); 
+    setTimeout(Etape2, 1500);
+    //afficher la fonction PhraseFinale après 3sec = le texte change de couleur et se met dans l'ordre
+    setTimeout(PhraseFinale, 3000);
     }
 
 //fonction pour afficher le texte
 function displayText(){
-    fill(color);
-    textSize(38);
-    text(choisi,x,y);
+    for (let i=0; i<x.length; i++){
+        fill(color);
+        textSize(38);
+        text(choisi[i],x[i],y[i]);
+    }
 }
 
 //fonction pour choisir la couleur du texte et modifier sa position
@@ -50,6 +58,8 @@ function Etape2(){
     text(choisi,x = x2,y = y2);
 }
 
-function TextChoisi(){
-    
+//changer la couleur du texte et mettre la phrase dans l'ordre final
+function PhraseFinale(){
+    color = "#A60076";
+    text(choisi, x=xf, y=yf);
 }
