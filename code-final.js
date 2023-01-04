869e1a293e1827c781c1e4681c359fc5a9af01a0
Astryal7 <n.lecardeux@gmail.com>
2023-01-04 17:44:37 +0100

    .

--- code-final.js
+++ code-final.js
@@ -107,18 +107,15 @@ class Word {
             this.ty = this.y
             this.idx = idx
         }
-
         spread() {
             this.tx = random(windowWidth)
             this.ty = random(windowHeight)
         }
-
         update() {
             //se déplace vers la cible de 10% à chaque fois
             this.x = lerp(this.x, this.tx, 0.1)
             this.y = lerp(this.y, this.ty, 0.1)
         }
-    
         display() {
             fill(color1)
             noStroke()
@@ -133,7 +130,7 @@ function Disappear(){
 //fonction pour choisir la couleur du texte et modifier sa position
 function Etape2(){
     color = "#00A676";
-    text(choisi,x = x2,y = y2);
+    text(choisi,x ,y ); 
 }
 
 //changer la couleur du texte et mettre la phrase dans l'ordre final
